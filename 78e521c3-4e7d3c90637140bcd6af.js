2abd04361c75422db2a745122ecfd17911c0969c
Berlin Bakers Against Racism <berlinbakersagainstracism.data@gmail.com>
2020-06-15 21:47:40 +0200

    Updates

---
+++ 78e521c3-4e7d3c90637140bcd6af.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{v7au:function(a,t,n){"use strict";n.d(t,"a",(function(){return e}));var i=n("Lnxd"),e=function(a){return Object(i.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{fillRule:"nonzero",d:"M13 7v4h7a1 1 0 0 1 1 1v8h2v2H1v-2h2v-8a1 1 0 0 1 1-1h7V7h2zm6 6H5v7h14v-7zM13.83.402A3 3 0 0 1 12.732 4.5L11 5.5a3 3 0 0 1 1.098-4.098l1.732-1z"}}]}]})(a)};e.displayName="RiCakeLine"}}]);
+//# sourceMappingURL=78e521c3-4e7d3c90637140bcd6af.js.map
