5dd4f881dcf088d5597e5da7fe71e12c0ae2c74c
Berlin Bakers Against Racism <berlinbakersagainstracism.data@gmail.com>
2020-06-17 02:36:22 +0200

    Fixes React warnings with nested <p>s

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -59,44 +59,46 @@ const OrderForm: React.FC<{ onSubmit: (donor: Donor) => Promise<OrderResponse> }
       {({ submitForm, isSubmitting }) => (
         <Grid container>
           <Grid item xs={12}>
-            <Typography variant="body1">
+            <Typography variant="body1" component="span">
               <p>Hi Berliners!</p>
-              <p>Together with 20+ bakers in the city, Berlin Bakers Against Racism is hosting a bake sale
+              <p>
+                Together with 20+ bakers in the city, Berlin Bakers Against Racism is hosting a bake sale
               to raise funds to fight against systemic racism. 100% proceeds donated to
                 &nbsp;<Link href="https://eji.org/" target="_blank" rel="noopener">Equal Justice Initiative</Link> and
-                &nbsp;<Link href="https://kop-berlin.de/" target="_blank" rel="noopener">KOP-Berlin</Link>.</p>
-              <p>Bake sale is now open and taking orders until end-of-day Wednesday 17. June. Contribute to the fight for justice
-                AND satisfy your sweet tooth! Limited goods available - so hurry before they’re gone!</p>
-              <p>Bakers Against Racism is an initiative started in the US by
+                &nbsp;<Link href="https://kop-berlin.de/" target="_blank" rel="noopener">KOP-Berlin</Link>.
+              </p>
+              <p>
+                Bake sale is now open and taking orders until end-of-day Wednesday 17. June. Contribute to the fight for justice
+                AND satisfy your sweet tooth! Limited goods available - so hurry before they’re gone!
+              </p>
+              <p>
+                Bakers Against Racism is an initiative started in the US by
                 &nbsp;<Link href="https://www.instagram.com/smallorchids" target="_blank" rel="noopener">@smallorchids</Link>
                 &nbsp;<Link href="https://www.instagram.com/badwolf_88" target="_blank" rel="noopener">@badwolf_88</Link>
                 &nbsp;&amp;&nbsp;
                 <Link href="https://www.instagram.com/robrubba" target="_blank" rel="noopener">@robrubba</Link>&nbsp;
-                to fight for justice. Their initiative is now in 15 countries world-wide including here in Berlin!</p>
+                to fight for justice. Their initiative is now in 15 countries world-wide including here in Berlin!
+              </p>
+            </Typography>
+            <Typography variant="body1">
+              <Link href="https://www.instagram.com/berlinbakersagainstracism" target="_blank" rel="noopener">
+                <InstagramIcon fontSize="large" style={{ verticalAlign: "middle" }} />
+                @berlinbakersagainstracism
+              </Link>
+              &nbsp;&nbsp;&nbsp;
+              <Link href="https://www.instagram.com/bakrsagainstracism" target="_blank" rel="noopener">
+                <InstagramIcon fontSize="large" style={{ verticalAlign: "middle" }} />
+                @bakersagainstracism
+              </Link>
             </Typography>
             <br />
-            <p>
-              <Typography variant="body1">
-                <Link href="https://www.instagram.com/berlinbakersagainstracism" target="_blank" rel="noopener">
-                  <InstagramIcon fontSize="large" style={{ verticalAlign: "middle" }} />
-                  @berlinbakersagainstracism
-                </Link>
-                &nbsp;&nbsp;&nbsp;
-                <Link href="https://www.instagram.com/bakrsagainstracism" target="_blank" rel="noopener">
-                  <InstagramIcon fontSize="large" style={{ verticalAlign: "middle" }} />
-                  @bakersagainstracism
-                </Link>
-              </Typography>
-            </p>
-            <p>
-              <Typography variant="body1">
-                &nbsp;<Link href="https://www.instagram.com/explore/tags/bakersagainstracism" target="_blank" rel="noopener">#bakersagainstracism</Link>
-                &nbsp;<Link href="https://www.instagram.com/explore/tags/berlinbakersagainstracism" target="_blank" rel="noopener">#berlinbakersagainstracism</Link>
-                &nbsp;<Link href="https://www.instagram.com/explore/tags/blacklivesmatter" target="_blank" rel="noopener">#blacklivesmatter</Link>
-                &nbsp;<Link href="https://www.instagram.com/explore/tags/blacklivesmatterberlin" target="_blank" rel="noopener">#blacklivesmatterberlin</Link>
-                &nbsp;<Link href="https://www.instagram.com/explore/tags/blmberlin" target="_blank" rel="noopener">#blmberlin</Link>
-              </Typography>
-            </p>
+            <Typography variant="body1">
+              &nbsp;<Link href="https://www.instagram.com/explore/tags/bakersagainstracism" target="_blank" rel="noopener">#bakersagainstracism</Link>
+              &nbsp;<Link href="https://www.instagram.com/explore/tags/berlinbakersagainstracism" target="_blank" rel="noopener">#berlinbakersagainstracism</Link>
+              &nbsp;<Link href="https://www.instagram.com/explore/tags/blacklivesmatter" target="_blank" rel="noopener">#blacklivesmatter</Link>
+              &nbsp;<Link href="https://www.instagram.com/explore/tags/blacklivesmatterberlin" target="_blank" rel="noopener">#blacklivesmatterberlin</Link>
+              &nbsp;<Link href="https://www.instagram.com/explore/tags/blmberlin" target="_blank" rel="noopener">#blmberlin</Link>
+            </Typography>
             <br />
           </Grid>
           <Form>
